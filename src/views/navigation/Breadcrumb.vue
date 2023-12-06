475a8ace1948479d779ed44269648c29217e22ef
zhensg123 <xyqblog@163.com>
2023-12-06 11:42:57 +0800

    提交

--- src/views/navigation/Breadcrumb.vue
+++ src/views/navigation/Breadcrumb.vue
@@ -1,8 +1,8 @@
 <template>
-    <el-breadcrumb separator="/">
-  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-  <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-  <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-  <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-</el-breadcrumb>
+    <unit-breadcrumb separator="/">
+  <unit-breadcrumb-item :to="{ path: '/' }">首页</unit-breadcrumb-item>
+  <unit-breadcrumb-item><a href="/">活动管理</a></unit-breadcrumb-item>
+  <unit-breadcrumb-item>活动列表</unit-breadcrumb-item>
+  <unit-breadcrumb-item>活动详情</unit-breadcrumb-item>
+</unit-breadcrumb>
 </template>
